2d4636cc37a6d336f0285061d40157fedeaecf6c
Christian Riggi <christian.riggi@ontariotechu.net>
2021-04-11 18:54:29 -0400

    Pulled Jonathan's Updated Version

--- OTTER-master/projects/CG A3/src/main.cpp
+++ OTTER-master/projects/CG A3/src/main.cpp
@@ -474,7 +474,7 @@ int main() {
 			illuminationBuffer = &illuminationbufferObject.emplace<IlluminationBuffer>();
 			illuminationBuffer->Init(width, height);
 		}
-
+		
 		int shadowWidth = 4096;
 		int shadowHeight = 4096;
 
@@ -632,8 +632,11 @@ int main() {
 			
 			obj3.get<Transform>().SetLocalPosition(currentPos.x, currentPos.y, currentPos.z);
 
+<<<<<<< Updated upstream
 			//Sets the icosphere to the transform of the light direction
 			Light.get<Transform>().SetLocalPosition(illuminationBuffer->GetSunRef()._lightDirection);
+=======
+>>>>>>> Stashed changes
 
 			//Lighting calculations for lighting volumne
 			//This will allow it so that fragments only have light applied to them when something passes through the mesh.
@@ -647,6 +650,11 @@ int main() {
 
 			illuminationBuffer->SetRadius(radius);
 
+			//Sets the icosphere to the transform of the light direction
+			obj10.get<Transform>().SetLocalPosition(illuminationBuffer->GetSunRef()._lightDirection);
+			//Stes the icosphere to the scale of the light's radius
+			obj10.get<Transform>().SetLocalScale(radius, radius, radius);
+			
 			if (forwards)
 			{
 				obj3.get<Transform>().SetLocalRotation(90.0f, 0.0f, 90.0f);
